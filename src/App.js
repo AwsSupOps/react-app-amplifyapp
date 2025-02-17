94b31f4302a3849062273188d14eb219c5ee23e9
EC2 Default User <ec2-user@ip-172-31-12-64.ec2.internal>
2025-02-17 16:18:03 +0000

    first commitdev6

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> testing preview witha mplify    dev4
-      dev5
+          Edit <code>src/App.js</code> testing preview witha mplify    dev6
           
           
         </p>
